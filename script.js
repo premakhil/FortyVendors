8f9e8a1d707e16ab2963a4c7b4ae108649f759cf
premakhil <akhilpchand@gmail.com>
2021-05-20 18:30:41 +0530

    Added animations

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 $("#learn-more-btn").click(
     function () {
+        $("#learn-more-btn").removeClass("active-color");
         $(this).addClass("active-color");
     }
 )
+
+
+
+
+
+
+$(function () {
+    new WOW().init();       // Initiate the wowjs
+});
+
+$('.navbar-collapse a').click(function () {    //Collapse navbar on click 
+    $(".navbar-collapse").collapse('hide');
+});
