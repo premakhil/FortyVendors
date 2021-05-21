56b34497eddcd13d8af0367f1650b57b3c51f8df
premakhil <akhilpchand@gmail.com>
2021-05-22 00:43:14 +0530

    scroll-top button added

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,8 +22,27 @@ $('.navbar-collapse button').click(function () {    //Collapse navbar on clickin
 
 
 function scroller() {
-    var elem = document.getElementById("footer-class");
+    var elem = document.getElementById("footer-class");         //contact-button-scrolling
     elem.scrollIntoView();
+}
+
+
+
+
+mybutton = document.getElementById("movetop");
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {      //scroll-top button
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;                                      // For Safari
+    document.documentElement.scrollTop = 0;                          // For Chrome, Firefox, IE and Opera
 }
 
 
